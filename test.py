33e4b862584b5e8d14638a7394049da1b5063f62
yinghuang <yinghuang@tuputech.com>
2017-09-25 12:24:27 +0800

    add test

--- test.py
+++ test.py
@@ -55,6 +55,21 @@ def compute(batch, weight, fc_act):
     context.pop()
     return fc_act
     
+
+def is_equal(a,b,method='any',epsilon=0.001):
+    '''
+    Judge whether a and b matrix is equal
+    '''
+    if not (a.shape == b.shape):
+        return False
+
+    if method == 'all':
+        return (a==b).all()
+    elif method == 'any':
+        return not (((a-b)>epsilon).any())
+    else:
+        raise ValueError("equal compare type %s is invalid,should by all or any" % method)
+
 def test_1(batch_size,num_output,feature_dim):
     batch = np.arange(feature_dim*batch_size).reshape((feature_dim, batch_size)).astype(np.float32)
     weight = np.arange(feature_dim*num_output).reshape((feature_dim,num_output)).astype(np.float32)
@@ -68,19 +83,16 @@ def test_1(batch_size,num_output,feature_dim):
     #real_dis.astype(np.int64)
     #print('real dis shape %s data is %s' % (real_dis.shape,real_dis))
     assert (distance==real_dis).all(),"test_1 batch_size %d, num_ouput %d, feature_dim %d failed!!!" % (batch_size,num_output,feature_dim)
-    if context:
-        context.pop()
-
+    
 def test_2(batch_size,num_output,feature_dim):
+    print('Current test for batch size %d, num_output %d, feature_dim %d' % (batch_size, num_output, feature_dim)) 
     batch = np.random.random((feature_dim, batch_size)).astype(np.float32)
     weight = np.random.random((feature_dim, num_output)).astype(np.float32)
     distance = np.zeros((batch_size,num_output), np.float32)
 
     with Timer('Test 2 Gpu matmult'):
         distance = compute(batch,weight, distance)
-    if context:
-        context.pop()
-
+    
 
     with Timer('Test 2 numpy mult'):
         real_dis = np.dot(batch.T,weight)
@@ -90,22 +102,35 @@ def test_2(batch_size,num_output,feature_dim):
     
     from mxnet import nd
     import mxnet as mx
-    nd_batch_transpose = nd.array(batch.T, mx.gpu(gpu_id))
-    nd_weight = nd.array(weight, mx.gpu(gpu_id))
-    with Timer('Test 2 mxnet dot compute'):
-        nd_dis = nd.dot(nd_batch_transpose, nd_weight)
-    #print('mxnet dis %s' % nd_dis)
+    nd_batch_gpu_t = nd.array(batch.T, mx.gpu(gpu_id))
+    nd_weight_gpu = nd.array(weight, mx.gpu(gpu_id))
+    with Timer('Test 2 mxnet GPU dot compute'):
+        nd_dis_gpu = nd.dot(nd_batch_gpu_t, nd_weight_gpu)
+    #print('mxnet dis %s' % nd_dis_gpu)
+
+    nd_batch_cpu_t = nd.array(batch.T, mx.cpu())
+    nd_weight_cpu = nd.array(weight, mx.cpu())
+    with Timer('Test 2 mxnet cpu dot'):
+        nd_dis_cpu = nd.dot(nd_batch_cpu_t, nd_weight_cpu)
     
-    #assert (distance==real_dis).all(), "test_2 batch_size %d, num_ouput %d, feature_dim %d failed equal elem num %d!!!" % (batch_size,num_output,feature_dim, np.sum(distance==real_dis))
-    #assert (nd_dis.asnumpy().astype(np.float32) == real_dis).all(), "Mxnet dist not equal to numpy dis!!!"
-    assert (nd_dis.asnumpy().astype(np.float32) == distance).all(), "Mxnet dist is not equal to gpu mat mult dis!!!"
+    assert is_equal(distance, real_dis), "Gpu dist is not equal numpy dis!!!" 
+    assert is_equal(nd_dis_gpu.asnumpy().astype(np.float32), real_dis), "Mxnet GPU dist not equal to numpy dis!!!"
+    assert is_equal(nd_dis_gpu.asnumpy().astype(np.float32), distance), "Mxnet nd gpu dist is not equal to gpu mat mult dis!!!"
+    assert is_equal(nd_dis_cpu.asnumpy().astype(np.float32), real_dis), "Mxnet CPU dist not equal to numpy dis!!!"
+    print('test ok')
 
 
-def test_list():
-    pass
+def test_cases():
+    for feature_dim in [128,256,512]:
+        for output_dim in [96000, 192000, 288000, 384000]:
+            test_2(PAD_QUERY_NUM, output_dim, feature_dim)
 
 #test_1(PAD_QUERY_NUM,96,512)
-test_2(PAD_QUERY_NUM,96000,512)
+test_2(PAD_QUERY_NUM, 96000, 512)
+test_cases()
+
+if context:
+    context.pop()
 
 print('run ok\n')
 
